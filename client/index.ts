838a94ba681072d10247323880c347669db2341a
shangxueink <1919892171@qq.com>
2025-12-11 16:45:43 +0800

    init

---
+++ client/index.ts
@@ -0,0 +1,15 @@
+import { Context, icons } from '@koishijs/client'
+
+import icom from './icon/icon.vue'
+import layout from './layout.vue'
+icons.register('glyph', icom)
+
+export default (ctx: Context) => {
+  ctx.page({
+    name: '字体管理',
+    path: '/glyph',
+    icon: 'glyph',
+    component: layout,
+    fields: ['glyph'],
+  })
+}
